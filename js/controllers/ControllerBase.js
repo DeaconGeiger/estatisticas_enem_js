636b5e1ff99fb93703d9c3362473a0c5e46094e8
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-09-29 12:35:59 -0300

    feat: funcionalidade para voltar à página inicial

--- js/controllers/ControllerBase.js
+++ js/controllers/ControllerBase.js
@@ -15,8 +15,58 @@ export default class ControllerBase {
     elementosCena.map(elemento => this.#cena.appendChild(elemento));
   }
 
-  voltarAoInicioBase(event) {
+  voltarAoInicioBase(evento) {
     this.#cena = new TelaInicial();
+  }
+
+  iniciarAnaliseBase(evento) {
+    const barraAnalise = document.getElementById("barra-analise");
+    const menuPeriodoInicial = document.getElementById("periodo-inicial");
+    const menuPeriodoFinal = document.getElementById("periodo-final");
+    const conteudoMenuPeriodoInicial = menuPeriodoInicial.lastChild;
+    const conteudoMenuPeriodoFinal = menuPeriodoFinal.lastChild;
+    let anoPeriodoInicial;
+    let anoPeriodoFinal;
+    if (evento.target.innerText == "Modelo Novo") {
+      anoPeriodoInicial = 1998;
+      anoPeriodoFinal = 2008;
+    } else {
+      anoPeriodoInicial = 2009;
+      anoPeriodoFinal = 2022;
+    }
+
+    for (let ano = anoPeriodoInicial; ano <= anoPeriodoFinal; ano++) {
+      const itens = [document.createElement("a"), document.createElement("a")];
+
+      itens.forEach(item => {
+        item.classList.add("item-menu");
+        item.innerText = `${ano}`;
+      });
+
+      conteudoMenuPeriodoInicial.appendChild(itens[0]);
+      conteudoMenuPeriodoFinal.appendChild(itens[1]);
+    }
+
+    this.configurarMenu(menuPeriodoInicial);
+    this.configurarMenu(menuPeriodoFinal);
+
+    barraAnalise.style.visibility = "visible";
+  }
+
+  periodoValido(periodoInicial, periodoFinal) {
+    try {
+      const diferencaPeriodo = periodoFinal - periodoInicial;
+      if (diferencaPeriodo < 0)
+        throw new TypeError("O período final não pode ser menor que o período inicial!");
+      else if (diferencaPeriodo == 0) {
+        throw new TypeError("A diferença entre os períodos deve ser de pelo menos 1 ano!");
+      }
+    } catch (erro) {
+      alert("Erro!\n\n" + erro.message);
+      return false;
+    }
+
+    return true;
   }
 
   configurarMenu(menu) {
@@ -44,6 +94,6 @@ export default class ControllerBase {
           botao.classList.remove("menu-ativo");
         })
       }
-    })
+    });
   }
 }
