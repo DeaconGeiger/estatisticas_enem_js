1c1b56681ea948a569709408ef0c4ac1aed4c261
Davi Bittencourt de Almeida <dbittencourt2002@gmail.com>
2023-09-29 10:11:14 -0300

    refactor: ajustes na função definirCena()

--- js/controllers/ControllerBase.js
+++ js/controllers/ControllerBase.js
@@ -10,11 +10,40 @@ export default class ControllerBase {
   }
 
   definirCena(elementosCena) {
-    this.#cena = document.getElementById("tela")
-    elementosCena.map(elemento => this.#cena.appendChild(elemento))
+    this.#cena = document.getElementById("tela");
+    this.#cena.innerHTML = '';
+    elementosCena.map(elemento => this.#cena.appendChild(elemento));
   }
 
   voltarAoInicioBase(event) {
     this.#cena = new TelaInicial();
+  }
+
+  configurarMenu(menu) {
+    const botaoMenu = menu.firstChild;
+    const conteudoMenu = menu.lastChild;
+    botaoMenu.addEventListener("click", () => {
+      conteudoMenu.classList.toggle("menu-ativo");
+    });
+
+    const itensMenu = Array.from(conteudoMenu.children);
+    itensMenu.map(item => {
+      item.addEventListener("click", () => {
+        botaoMenu.innerText = item.innerText;
+        conteudoMenu.classList.remove("menu-ativo");
+      })
+    })
+  }
+
+  configurarFechamentoMenus() {
+    window.addEventListener("click", evento => {
+      if (!evento.target.matches(".botao-menu")) {
+        const botoes = Array.from(document.getElementsByClassName("conteudo-menu"));
+
+        botoes.map(botao => {
+          botao.classList.remove("menu-ativo");
+        })
+      }
+    })
   }
 }
